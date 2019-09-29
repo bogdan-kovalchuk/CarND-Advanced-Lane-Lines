caf3211c6c7396e4ef74674c075548b2c2a50323
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2019-09-29 18:57:00 +0300

    Threshold functions

--- thresholds.py
+++ thresholds.py
@@ -29,8 +29,8 @@ def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     return binary
 
 
-def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
-    thresh_min, thresh_max = mag_thresh
+def mag_thresh(image, sobel_kernel=3, thresh=(0, 255)):
+    thresh_min, thresh_max = thresh
     # 1) Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
@@ -52,7 +52,8 @@ def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
     # 6) Return this mask as your binary_output image
     return binary
 
-def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+
+def dir_thresh(image, sobel_kernel=3, thresh=(0, np.pi / 2)):
     thresh_min, thresh_max = thresh
 
     # 1) Convert to grayscale
@@ -72,27 +73,67 @@ def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
     return binary
 
 
+def hls_threshold(image, thresh=(0, 255)):
+    # 1) Convert to HLS color space
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    S = hls[:, :, 2]
+
+    # 2) Apply a threshold to the S channel
+    binary = np.zeros_like(S)
+    binary[(S > thresh[0]) & (S <= thresh[1])] = 1
+
+    # 3) Return a binary image of threshold result
+    return binary
+
+
+def pipeline(image, ksize, s_thresh, sx_thresh, sy_thresh, m_thresh, d_thresh):
+    image = np.copy(image)
+
+    # Sobel threshold
+    sxbinary = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=sx_thresh)
+    sybinary = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=sy_thresh)
+
+    # Magnitude of the gradient threshold
+    mag_binary = mag_thresh(image, sobel_kernel=ksize, thresh=m_thresh)
+
+    # Direction of the gradient threshold
+    dir_binary = dir_thresh(image, sobel_kernel=ksize, thresh=d_thresh)
+
+    # S color channel threshold
+    s_binary = hls_threshold(img, thresh=s_thresh)
+
+    # Combined binary
+    combined = np.zeros_like(dir_binary)
+    combined[((sxbinary == 1) & (sybinary == 1)) | ((mag_binary == 1) & (dir_binary == 1)) | (s_binary == 1)] = 1
+
+    return combined
+
+
 if __name__ == "__main__":
-    image = mpimg.imread('signs_vehicles_xygrad.png')
+    img = mpimg.imread('bridge_shadow.jpg')
 
     # Choose a Sobel kernel size
     ksize = 3  # Choose a larger odd number to smooth gradient measurements
+    s_thresh = (170, 255)
+    sx_thresh = (20, 100)
+    sy_thresh = (20, 255)
+    m_thresh = (30, 100)
+    d_thresh = (0.7, 1.3)
 
-    # Apply each of the thresholding functions
-    gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
-    grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 255))
-    mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
-    dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.3))
-
-    combined = np.zeros_like(dir_binary)
-    combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+    binary = pipeline(image=img,
+                      ksize=ksize,
+                      s_thresh=s_thresh,
+                      sx_thresh=sx_thresh,
+                      sy_thresh=sy_thresh,
+                      m_thresh=m_thresh,
+                      d_thresh=d_thresh)
 
     # Plot the result
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
     f.tight_layout()
-    ax1.imshow(image)
+    ax1.imshow(img)
     ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(combined, cmap='gray')
+    ax2.imshow(binary, cmap='gray')
     ax2.set_title('Thresholded Grad. Dir.', fontsize=50)
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.show()
